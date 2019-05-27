ef050d8c57865781fd08d15c89f4067af03bc453
patriciaann1493 <51077053+patriciaann1493@users.noreply.github.com>
2019-05-27 21:04:15 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 
-http.createServer(function (request, response) {
-   // Send the HTTP header 
-   // HTTP Status: 200 : OK
-   // Content Type: text/plain
-   response.writeHead(200, {'Content-Type': 'text/plain'});
-   
-   // Send the response body as "Hello World"
-   response.end('Hello World\n');
-}).listen(8080);
+'use strict';
 
+const express = require('express');
+
+// Constants
+const PORT = 8080;
+const HOST = '10.98.252.178';
+
+// App
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello world\n');
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
+var http = require("http");
